d94674ad15f71d005c364091e96fe9215d909c3e
Scott Haluck <sjhaluck@gmail.com>
2014-07-31 13:30:29 -0700

    created a table

---
+++ sqlapp.py
@@ -0,0 +1,14 @@
+# import sqlite3 library
+import sqlite3
+
+# create new database if the database doesn't already exist
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("""CREATE TABLE population (city TEXT, state TEXT, population INT)""")
+
+# close the database connection
+conn.close()
